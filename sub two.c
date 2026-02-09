0bdf2e268e6d7b41e1b8f5376c60ac9c2512628a
subashreesakthivel17-sakthi <subashreesakthivel17@gmail.com>
2026-02-09 15:30:56 +0530

    Add files via upload

---
+++ sub two.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+int main(){
+    int a,b;
+    scanf("%d%d",&a,&b);
+    printf("%d",a-b);
+    int c,d;
+    scanf("%d%d",&c,&d);
+    printf("%d",c-d);
+    int e,f;
+    scanf("%d%d",&e,&f);
+    printf("%d",e-d);
+    return 0;
+}
+    
