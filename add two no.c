4f411710bb5615ec41338ccf85977d33b8055144
subashreesakthivel17-sakthi <subashreesakthivel17@gmail.com>
2026-02-09 15:26:36 +0530

    Add files via upload

---
+++ add two no.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+int main(){
+    int a,b;
+    scanf("%d%d",&a,&b);
+    printf("%d",a+b);
+    int c,d;
+    scanf("%d%d",&c,&d);
+    printf("%d",c+d);
+    return 0;
+}
+    
