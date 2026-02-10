a539a08966e9665213a9989ff42f424d1d832889
subashreesakthivel17-sakthi <subashreesakthivel17@gmail.com>
2026-02-10 13:25:27 +0530

    Add files via upload

---
+++ assgn div,mod.c
@@ -0,0 +1,22 @@
+
+#include <stdio.h>
+
+int main()
+{
+    int a;
+     a=9;
+     a/=2;
+    printf("%d ",a);
+    a=9;
+    a%=2;
+    printf("%d\n",a);    
+    int b;
+    b=10;
+    b/=5;
+    printf("%d ",b);
+    b=10;
+    b%=5;
+    printf("%d",b);
+    return 0;
+}
+   
