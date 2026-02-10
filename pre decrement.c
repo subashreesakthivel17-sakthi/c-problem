6b33a1d00fcf1772879417cd8c2310895e2551fb
subashreesakthivel17-sakthi <subashreesakthivel17@gmail.com>
2026-02-10 11:06:53 +0530

    Add files via upload

---
+++ pre decrement.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+int main()
+{
+    int a;\\10
+    scanf("%d",&a);
+    printf("%d",--a);
+    int b;\\1
+    scanf("%d",&b);
+    printf("%d",--b);
+
+    return 0;
+}
+O\p
+9
+0
