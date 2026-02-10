8fa0fd4b9c6896a98eb68e192f22a71e3b8c283c
subashreesakthivel17-sakthi <subashreesakthivel17@gmail.com>
2026-02-10 10:54:51 +0530

    Add files via upload

---
+++ add three.c
@@ -0,0 +1,14 @@
+
+#include <stdio.h>
+
+int main()
+{
+    int a,b,c;
+    scanf("%d %d %d",&a,&b,&c);
+    printf("%d",a+b+c);
+    int d,e,f;
+    scanf("%d%d%d",&d,&e,&f);
+    printf("%d",d+e+f);
+
+    return 0;
+}
