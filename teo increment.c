ba3c0cfc0a995d38509f0352769ecd19f8b84575
subashreesakthivel17-sakthi <subashreesakthivel17@gmail.com>
2026-02-10 11:11:40 +0530

    Add files via upload

---
+++ teo increment.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+int main()
+{
+    int a;//a=5
+    scanf("%d",&a);
+    printf("%d",++a,++a);
+    int b;//b=0
+    scanf("%d",&b);
+    printf("%d",++b,++b);
+
+    
+
+    return 0;
+}
