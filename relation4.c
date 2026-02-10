e0800068fa4f9fe12040bcd30dd449a26c54a851
subashreesakthivel17-sakthi <subashreesakthivel17@gmail.com>
2026-02-10 11:31:42 +0530

    Add files via upload

---
+++ relation4.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+int main()
+{
+    int a;//5
+    int b;//3
+    scanf("%d %d",&a,&b);
+    printf("%d",a!=b);
+    int c,d;//4,4
+    scanf("%d %d",&c,&d);
+    printf("%d",c!=d);
+    return 0;
+}
