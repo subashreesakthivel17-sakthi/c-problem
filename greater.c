6197df2d27383d353f136b749df443c4dfbb7357
subashreesakthivel17-sakthi <subashreesakthivel17@gmail.com>
2026-02-11 11:39:42 +0530

    Add files via upload

---
+++ greater.c
@@ -0,0 +1,17 @@
+
+#include <stdio.h>
+
+int main()
+{
+   int a=20;
+   int b=10;
+   printf("%d",a ,b);
+   if(a>b){
+       printf(" is greater");
+   }
+   else{
+       printf(" is not greater");
+   }
+
+    return 0;
+}
