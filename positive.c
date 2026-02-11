35ad5db59d2ad1ecf307d29c3bc48eb02d70bc4c
subashreesakthivel17-sakthi <subashreesakthivel17@gmail.com>
2026-02-11 10:51:31 +0530

    Add files via upload

---
+++ positive.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+int main()
+{
+   int a;//5
+   scanf("%d",&a);
+   if(a > 0){
+       printf("positive number");
+     }
+    else{
+        printf("negative number");
+    }
+   
+
+    return 0;
+}
