d084d153a503b37d4c0940f1252224eccb6fd52f
subashreesakthivel17-sakthi <subashreesakthivel17@gmail.com>
2026-02-11 15:35:26 +0530

    Add files via upload

---
+++ upper case.c
@@ -0,0 +1,16 @@
+
+#include <stdio.h>
+
+int main()
+{
+    char ch='G';
+    
+    if(ch>='A'&& ch<='Z'){
+        printf("upper case");
+    }
+    else{
+        printf("lower case");
+    }
+
+    return 0;
+}
