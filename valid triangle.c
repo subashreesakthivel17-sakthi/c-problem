d1f357118a5aa771840767b6a8499d10cb18e8c1
subashreesakthivel17-sakthi <subashreesakthivel17@gmail.com>
2026-02-11 15:51:42 +0530

    Add files via upload

---
+++ valid triangle.c
@@ -0,0 +1,15 @@
+
+#include <stdio.h>
+
+int main()
+{
+  int a=60,b=60,c=60;
+   int sum = a+b+c;
+  if(sum==180){
+      printf("Valid triangle");
+  }
+  else{
+      printf("not valid triangle");
+  }
+    return 0;
+}
