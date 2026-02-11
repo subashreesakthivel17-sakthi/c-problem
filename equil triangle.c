e2e906f01ec73198a43b7a79a06ad1a8feca5c2c
subashreesakthivel17-sakthi <subashreesakthivel17@gmail.com>
2026-02-11 15:54:37 +0530

    Add files via upload

---
+++ equil triangle.c
@@ -0,0 +1,15 @@
+
+#include <stdio.h>
+
+int main()
+{
+  int a=5,b=5,c=5;
+   
+  if(a==b&&a==c){
+      printf("equilateral triangle");
+  }
+  else{
+      printf("not equilateral triangle");
+  }
+    return 0;
+}
