53d397ec0df9e1f75685b9f94621acedd253d112
subashreesakthivel17-sakthi <subashreesakthivel17@gmail.com>
2026-02-11 14:20:55 +0530

    Add files via upload

---
+++ three digit no.c
@@ -0,0 +1,17 @@
+
+#include <stdio.h>
+
+int main()
+{
+    int a;
+    scanf("%d",&a);
+    if(a>=100&&a<=999){
+        printf("three digit number");
+    }
+    else{
+        printf("not three digit number");
+    }
+    
+   
+    return 0;
+}
