3b380e0e50e010cf1d968ceef1adadeb48174acb
subashreesakthivel17-sakthi <subashreesakthivel17@gmail.com>
2026-02-12 11:01:32 +0530

    Add files via upload

---
+++ vowel switch.c
@@ -0,0 +1,34 @@
+
+#include <stdio.h>
+
+int main()
+{
+    char ch;
+    scanf("%c",&ch);
+    switch(ch){
+        case 'A':
+        case 'a':
+        printf("Vowels");
+        break;
+        case 'E':
+        case 'e':
+        printf("Vowels");
+        break;
+        case 'I':
+        case 'i':
+        printf("Vowels");
+        break;
+        case 'O':
+        case'o':
+        printf("Vowels");
+        break;
+        case 'U':
+        case'u':
+        printf("Vowels");
+        break;
+        default:
+        printf("invalid");
+        
+    }
+}
+    
